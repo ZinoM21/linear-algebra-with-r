aae0f26d62340738e2c5cc2d5e5551ce36a85c04
Zino Meyer <zino.meyer@code.berlin>
2024-09-30 12:05:59 -0700

    exercise 1.8

--- assignments/a1-meyer-zino.R
+++ assignments/a1-meyer-zino.R
@@ -7,6 +7,71 @@
 darwin_data <- read.table("data/PSTANDdarwin.txt", header = FALSE)
 tahiti_data <- read.table("data/PSTANDtahiti.txt", header = FALSE)
 # ------------------------
+# 1.8
+# a)
+precip_data <- read.csv("data/3447060.csv", header = TRUE)
+
+# Create the pivot table with stations as rows, years as columns
+station_year_df <- xtabs(PRCP ~ STATION + DATE, data = precip_data)
+
+# Anomaly data X (with respect to mean), and Y columns for time in years
+mean_precip <- rowMeans(station_year_df)
+X <- sweep(station_year_df, 1, mean_precip)
+Y <- ncol(X)
+
+# covariance matrix formula: C = X * t(X) / Y
+cov_x <- (X %*% t(X)) / Y
+cov_x
+
+# b)
+inv_cov_x <- solve(cov_x)
+inv_cov_x
+
+# c)
+eig_cov_x <- eigen(cov_x)
+eig_cov_x$values
+eig_cov_x$vectors
+
+# d)
+x_svd <- svd(X)
+x_svd$u
+x_svd$d
+x_svd$v
+
+# e)
+eig_cov_x$values
+x_svd$d
+x_svd$d^2 / Y
+# -> the eigenvalues of the covariance matrix are
+# the squares of the singular values of the data matrix,
+# divided by the number of columns
+
+# f)
+eig_cov_x$vectors
+x_svd$u
+# -> The eigenvectors of the covariance matrix are the same
+# as the SVD spatial modes of X
+
+# g) Plot the PC time series and describe their behavior.
+V_t <- t(x_svd$v)
+V_t
+
+png("images/pc-time-series.png", width = 800, height = 600)
+plot(1:Y, V_t[1, ], type = "l", col = "red", xlab = "Year", ylab = "PC", main = "PC Time Series")
+lines(1:Y, V_t[2, ], type = "l", col = "blue")
+lines(1:Y, V_t[3, ], type = "l", col = "green")
+legend("topright", legend = c("1", "2", "3"), col = c("blue", "red", "green"), lty = 1)
+dev.off()
+
+# We see that lines one and three are very similar at the end (falling near
+# year 4 and rising for year 5).
+# Also, lines two and three are similar at the beginning (rising for year 1-2
+# and falling after year 3).
+# This describes / looks like an oscillation pattern, where all the lines
+# rise and fall from -0.6 to 0.5. We just see an excerpt from this
+# larger pattern.
+# I would guess that one oscillation cycle is roughly 6-8 years long.
+# ------------------------
 # 2.1 Write a computer code to
 # a) Read the NOAAGlobalTemp data file, and
 # b) Generate a 4 × 8 space-time data matrix for the December mean
